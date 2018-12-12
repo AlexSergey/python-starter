ce36a73022a4ddf76d9edcf62c764a50525d557a
Sergey Aleksandrov <gooddev.sergey@gmail.com>
2018-12-12 09:38:54 +0200

    update starter

--- README.md
+++ README.md
@@ -2,10 +2,13 @@
 
 This is fast-starter boilerplate.
 
-- Python3
-- Poet for DI
+- Python3.6
+- better_exceptions
+- loguru
 
 ## Install
 
 - virtualenv -p python3.6 python3-env
-- poet install
+- cd python3-env/bin
+- source activate
+- cd ../../
--- app.py
+++ app.py
@@ -1,4 +1,16 @@
 import better_exceptions
-from src.logger.index import logger
+from loguru import logger
+from string import Template
+import os
 
-logger.info('App started!')
+LOG = 'logs'
+
+if not os.path.exists(LOG):
+    os.makedirs(LOG)
+
+better_exceptions.hook()
+
+logger.start(Template("$logs/file_{time}.log").safe_substitute(logs=LOG))
+
+logger.debug("That's it, beautiful and simple logging!")
+
