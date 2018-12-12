aff73af50f9a423c1d9d849a278ed044c11f286d
Sergey Aleksandrov <gooddev.sergey@gmail.com>
2018-12-12 09:40:33 +0200

    remove logger

--- src/logger/index.py
+++
@@ -1,6 +0,0 @@
-import logging
-
-logging.basicConfig(filename='logging_app.log', level=logging.DEBUG, format='%(asctime)s - %(levelname)s - %(message)s\n-----------\n')
-logging.getLogger().addHandler(logging.StreamHandler())
-
-logger = logging
